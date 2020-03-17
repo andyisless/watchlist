7cec3b1f1cb7ca1c09f42ed30b64108c2280a08a
Andyisless <5251560@163.com>
2020-03-17 15:41:52 +0800

    Add base template and error template 404

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import url_for
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
 import click
+from flask import url_for
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -14,44 +14,18 @@ else:
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 #数据库创建这一步，不能用pycharm创建flask shell，会报错。
 #改为terminal创建即可
 
-@app.route('/')
-@app.route('/index')
-@app.route('/home')
-def index():
-    return render_template('index.html', name=name, movies=movies)
-
-def hello():
-    return 'Welcome to My Watchlist! <h1?My name is andy.</h1><img src="http://helloflask.com/totoro.gif">'
-
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % name
-
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例(请在命令行窗口查看输出的 URL):
-    print(url_for('hello'))
-    # 输出:/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli'))  # 输出:/user/gre yli
-    print(url_for('user_page', name='peter'))  # 输出:/user/peter
-
-    print(url_for('test_url_for'))  # 输出:/test     # 下面这个调用传入了多余的关键字参数,它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出:/test?num=2
-    return 'Test page'
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
-    year = db.Column(db.String(4))
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
 
 @app.cli.command()
 def forge():
@@ -80,9 +54,27 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
-@click.option('--drop', is_flag=True, help='Create after drop.')
-def initdb(drop):
-    if drop:
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route('/')
+def index():
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
